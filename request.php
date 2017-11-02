550e65d97300b7b3cf0f62b4091ec7d5627723c3
RachelleT <rache_trotman@hotmail.com>
2017-11-01 23:32:36 -0400

    Exercise 4-php

--- request.php
+++ request.php
@@ -4,7 +4,29 @@
 // respond with a value
 
 $query = $_GET['q'];
-$definition = [
+
+if (q == '&all=true'){
+    $xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+    <entries>
+    <definition name="definition" author="John">
+    a statement of the exact meaning of a word, especially in a
+    dictionary.
+    </definition>
+    <definition name="bar" author="mary">
+    a place that sells alcholic beverages
+    </definition>
+    <definition name="ajax" author="Kimberly">
+    technique which involves the use of javascript and xml
+    </definition>
+    </entries>';
+    
+    header('Content-Type: text/xml');
+    $xmlOutput = new SimpleXMLElement($xmldata);
+    echo $xmlOutput->asXML();
+}
+else
+{
+    $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "A place that sells alcholic beverages",
     "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
@@ -12,7 +34,8 @@ $definition = [
     "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
-];
+    ];
 
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+    print "<h3>" . strtoupper($query) . "</h3>";
+    print "<p>" . $definition[$query] . "</p>";
+}
